4d6de7b0e68171f82d1a1d299e1ff1c06ba5bdfc
Bergbok <66174189+Bergbok@users.noreply.github.com>
2025-01-21 12:55:47 +0200

    refactor: use IDs instead of classes on settings panel divs

--- src/components/Settings.ts
+++ src/components/Settings.ts
@@ -97,7 +97,7 @@ export const SettingsModal = () => {
     return {
         view: () => {
             return m('dialog#settings-dialog', [
-                m('button.close-button', { autofocus: true, onclick: toggleSettingsModal }, [
+                m('button#close-button', { autofocus: true, onclick: toggleSettingsModal }, [
                     m('svg.icon', { xmlns: 'http://www.w3.org/2000/svg', viewBox: '0 0 24 24', role: 'img', 'aria-label': 'close icon' }, [
                         m('path', { d: 'M5.72 5.72a.75.75 0 0 1 1.06 0L12 10.94l5.22-5.22a.749.749 0 0 1 1.275.326.749.749 0 0 1-.215.734L13.06 12l5.22 5.22a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215L12 13.06l-5.22 5.22a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042L10.94 12 5.72 6.78a.75.75 0 0 1 0-1.06Z' })
                     ])
@@ -115,13 +115,13 @@ export const SettingsModal = () => {
                     'Synchronous loading (much faster but might briefly freeze)'
                 ]),
                 m('h3', 'Theme'),
-                m('div.theme-choice', [
+                m('div#theme-choice', [
                     themes.map(theme => 
                         m(`button.circle#${theme}`, { onclick: () => changeSetting('theme', theme) })
                     )
                 ]),
                 m('h3', 'Font'),
-                m('div.font-choice', [
+                m('div#font-choice', [
                     fonts.map(font => 
                         m('label', { for: font.id }, [
                             m(`input#${font.id}`, {
