c69f26d8dbfb1f41eb2d6c3c4f731838199b136c
Bergbok <66174189+Bergbok@users.noreply.github.com>
2025-01-24 14:20:54 +0200

    refactor: increase amount of results displayed per page

--- src/components/Results.ts
+++ src/components/Results.ts
@@ -97,7 +97,7 @@ async function seekEmbed(videoID: string, second: number) {
 
 export const ResultsGrid = () => {
     const contextLevel = 1;
-    const itemsPerPage = window.innerWidth <= 768 ? 10 : 20;
+    const itemsPerPage = window.innerWidth <= 768 ? 20 : 100;
     const timestampRegex = new RegExp(/\[[\d:.]+\]/, 'g');
     let contextStart: number;
     let contextEnd: number;
