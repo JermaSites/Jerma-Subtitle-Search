658a022592cc3fd539008669580a5f2317b8f72c
Bergbok <66174189+Bergbok@users.noreply.github.com>
2025-01-24 13:07:06 +0200

    style: revise inconsistent indentation

--- src/components/ProgressBar.ts
+++ src/components/ProgressBar.ts
@@ -2,12 +2,12 @@ import m, { type Vnode } from 'mithril';
 import '../styles/ProgressBar.scss';
 
 export const ProgressBar = () => {
-  return {
-    view: (vnode: Vnode<{ max: number, value: number, message: string }>) => {
-        return m('div#progress-container', [
-            m('progress#progress-bar', { max: vnode.attrs.max, value: vnode.attrs.value }),
-            m('label#progress-label', { for: 'progress-bar' }, vnode.attrs.message)
-        ]);
-    }
-  };
+    return {
+        view: (vnode: Vnode<{ max: number, value: number, message: string }>) => {
+            return m('div#progress-container', [
+                m('progress#progress-bar', { max: vnode.attrs.max, value: vnode.attrs.value }),
+                m('label#progress-label', { for: 'progress-bar' }, vnode.attrs.message)
+            ]);
+        }
+    };
 };
