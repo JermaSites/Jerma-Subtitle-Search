f19e2af01c805548d3840c9f2a78e4561306d4e0
Bergbok <66174189+Bergbok@users.noreply.github.com>
2025-04-16 04:20:00 +0200

    feat: add MethodGaming & Jerma + Orb playlists to URL list

--- src/scripts/get_subtitles/get_subtitles.py
+++ src/scripts/get_subtitles/get_subtitles.py
@@ -3214,7 +3214,9 @@ if __name__ == '__main__':
         # 'https://youtube.com/playlist?list=PL9haG0G7kUOiKVQ-Iw7LO7fgQUG3xx2L9',       # Jerma Deleted Videos / Lost Content
         # 'https://youtube.com/playlist?list=PL_ahY5c4jVEAkFbpd6GniDf8g9pKRYHt-',       # Borderlands 2 Let's Play w/ Drak
         # 'https://youtube.com/playlist?list=PLLZVyAYonIO6NTJYz4-tIqxmhNZts1KjB'        # Jerma + Etalyx
+        # 'https://youtube.com/playlist?list=PLLZVyAYonIO7sNe6fKVMFFEglSyO8wolg',       # Jerma + Orb
         # 'https://youtube.com/playlist?list=PLLZVyAYonIO6lUcFTWyo6jfcNNvgtM3SZ',       # Jerma + Shibby
+        # 'https://youtube.com/playlist?list=PLLZVyAYonIO6m_Ht96IY82D1E_kPCkvRd',       # MethodGaming
         # 'https://archive.org/download/jerma-streams-public-domain-movie-night/PUBLIC%20DOMAIN%20MOVIE%20NIGHT%20PART%201.mp4'
     ], help='List of URLs to obtain subtitles for.')
     parser.add_argument('--cookies-path', type=str, default='cookies.txt', help='Netscape formatted file to read cookies from, can be obtained with: yt-dlp --cookies-from-browser firefox --cookies cookies.txt')
